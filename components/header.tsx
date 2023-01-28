c24e8b3e1112f1488e8a19987b129d2e3b10a0b7
a.daitoku <0718.asuka@gmail.com>
2023-01-28 20:35:06 +0900

    first commit

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <h1>
-        <Link className="neon"  href="/">
+        <Link className="flux"  href="/">
           {NEXT_PUBLIC_SITE_TITLE}
         </Link>
       </h1>
