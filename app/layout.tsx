9a42795f8373a6b8617ed6231b0630b8ae966956
ikarigata <0718.asuka@gmail.com>
2025-08-20 00:08:24 +0900

    不要になったインポートを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Header from '../components/header'
-import Footer from '../components/footer'
+// import Footer from '../components/footer'
 import '../styles/global.css'
 import '../styles/syntax-coloring.css'
 import styles from '../styles/shared.module.css'
